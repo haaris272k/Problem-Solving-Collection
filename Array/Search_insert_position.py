f358df8c11518ecf1fa667d963af24281ce25f7f
Mohammed Haaris Javed <89451392+haaris272k@users.noreply.github.com>
2022-02-22 23:14:34 +0530

    Add files via upload

---
+++ Array/Search_insert_position.py
@@ -0,0 +1,29 @@
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+
+
+"""
+
+nums = [1, 3, 5, 6]
+target = 2
+if target in nums:
+    print(nums.index(target))
+else:
+    nums += [target]
+    sorted_nums = sorted(nums)
+    print(sorted_nums.index(target))
