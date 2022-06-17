f0439fd9b0a4fd2cd090730f188e7f920599fd27
haaris272k <javedhaaris20@gmail.com>
2022-06-17 11:16:50 +0530

    Modified

---
+++ Bit Manipulation/CountingBits.py
@@ -0,0 +1,21 @@
+"""Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10"""
+
+
+# With in-Built Function
+n = 2
+binary_array = []
+for i in range(n + 1):
+    bin_rep = bin(i)
+    binary_array.append(bin_rep.count("1"))
+print(binary_array)
