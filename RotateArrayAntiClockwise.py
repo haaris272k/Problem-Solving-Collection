81174d8835b6ea2cd92f587f222f9419dabe2640
haaris272k <javedhaaris20@gmail.com>
2022-08-17 20:54:26 +0530

    Modified

--- RotateArrayAntiClockwise.py
+++ RotateArrayAntiClockwise.py
@@ -8,6 +8,7 @@ d = 2
 # to elements of array to the left/anit-clockwise dir by d
 # basic approach is to stre the elements which comes after d in a temp array
 # and elements from 0th idx to dth idx
+
 temp = []
 for i in range(len(arr)):
     temp = arr[d:] + arr[:d]
