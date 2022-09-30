20bebaf4ab938e7649cd21aa3baf126a26feab99
haaris272k <javedhaaris20@gmail.com>
2022-10-01 00:13:56 +0530

    Modified

--- HIGH PRIORITY SHEET/ARRAY/Searching/BinarySearch.py
+++ HIGH PRIORITY SHEET/ARRAY/Searching/BinarySearch.py
@@ -4,22 +4,16 @@ TC: O(log n)
 
 """
 
-nums = [4, 5, 6, 7, 0, 1, 2]
-target = 0
-low = 0
-high = len(nums) - 1
-for i in range(len(nums)):
-
-    mid = low + (high - low) // 2
-
+nums = [0, 1, 2, 7, 8, 13, 15]
+n = len(nums)
+target = 13
+l, r = 0, n - 1
+for i in range(n):
+    mid = (l + r) // 2
     if nums[mid] == target:
-
         print(mid)
-
-    elif nums[mid] < target:
-
-        low = mid + 1
+        break
+    elif nums[mid] > target:
+        r = mid - 1
     else:
-        high = mid - 1
-else:
-    print(-1)
+        l = mid + 1
