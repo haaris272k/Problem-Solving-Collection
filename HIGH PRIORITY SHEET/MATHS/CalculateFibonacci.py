4da03825db9da8309a7303e0b709eb5c108cfcff
haaris272k <javedhaaris272000@gmail.com>
2023-01-11 18:46:49 +0530

    Solved GFG POTD

--- HIGH PRIORITY SHEET/MATHS/CalculateFibonacci.py
+++ HIGH PRIORITY SHEET/MATHS/CalculateFibonacci.py
@@ -32,3 +32,12 @@ def fib(n: int) -> int:
 n = 4
 print(fib(n))
 
+
+def fibo(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+print(fibo(4))
