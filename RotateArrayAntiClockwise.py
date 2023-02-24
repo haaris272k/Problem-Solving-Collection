45e5885fb6e48b1874c7d508cb031e05603327a5
haaris272k <javedhaaris20@gmail.com>
2023-02-24 14:31:55 +0530

    added: runtime_tester

--- RotateArrayAntiClockwise.py
+++ RotateArrayAntiClockwise.py
@@ -1,18 +1,21 @@
 # arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 arr = [1, 2, 3, 4, 5]
 d = 2
+def rotateArray(arr, d):
+    # Approach 2
+    # TC: O(n)
+    # SC: O(1)
+    # to elements of array to the left/anit-clockwise dir by d
+    # basic approach is to stre the elements which comes after d in a temp array
+    # and elements from 0th idx to dth idx
 
-# Approach 1
-# TC: O(n)
-# SC: O(n)
-# to elements of array to the left/anit-clockwise dir by d
-# basic approach is to stre the elements which comes after d in a temp array
-# and elements from 0th idx to dth idx
+    temp = []
 
-temp = []
+    for i in range(len(arr)):
 
-for i in range(len(arr)):
+        temp = arr[d:] + arr[:d]
 
-    temp = arr[d:] + arr[:d]
+    return temp
 
-print(temp)
+
+print(rotateArray(arr, d))
---
+++ runtime.py
@@ -0,0 +1,21 @@
+import time
+
+def measure_runtime(func, *args, **kwargs):
+    """
+    Measure the runtime of a function with the given arguments.
+
+    Args:
+        func (callable): The function to be measured.
+        args: Positional arguments to be passed to the function.
+        kwargs: Keyword arguments to be passed to the function.
+
+    Returns:
+        The return value of the function.
+    """
+    start_time = time.time()
+    result = func(*args, **kwargs)
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"Runtime: {runtime:.6f} seconds")
+    return result
+
